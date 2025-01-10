3581ddd7fdc9f57248fc173dd57c2c862c6aa84c
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2025-01-10 11:33:25 +0500

    blog

---
+++ data/blogs.js
@@ -0,0 +1,24 @@
+blogs = [
+    {
+        title: "How to get started with Python",
+        content: "This is content Python",
+        slug: "Python learn"
+    },
+    {
+        title: "How to get started with JS",
+        content: "This is content JS",
+        slug: "Js-learn"
+    },
+    {
+        title: "How to get started with Django",
+        content: "This is content Django",
+        slug: "Django-learn"
+    },
+    {
+        title: "How to get started with CSS",
+        content: "This is content CSS",
+        slug: "CSS-learn"
+    }
+]
+
+module.exports = blogs;
